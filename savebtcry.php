c18be701274afb5fb077571b9f0c0ec97d810a0f
bitcrystal <dumm_heit@xyz.de>
2015-02-26 01:23:11 +0100

    Update savebtcry.php

--- savebtcry.php
+++ savebtcry.php
@@ -20,7 +20,7 @@ $iid = $my_coins->getSelectInstanceId();
 $cid = $my_coins->getCoinsSelectInstanceId();
 if($savemoney=="savemoney") {
 			$id=1+$cid;
-			$wallet_id = "zellesExchange(".$user_session.")";
+			$wallet_id = $my_coins->getWalletId($user_session,$Bitcrystald[$iid]["cid"]);
 			$FEEBEE = $my_coins->coins[$my_coins->coins_names[$id]]["FEEBEE"];
 			$wallet_id_feebee = "zellesExchange(".$FEEBEE.")";
 			$my_balance=userbalance($user_session,$my_coins->coins_names_prefix[$id]);
